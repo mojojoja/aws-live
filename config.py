02ee8147632ded7b08bf8c2c1b801bd3ba5b39ff
mojojoja <103723393+mojojoja@users.noreply.github.com>
2022-04-15 13:09:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cd5p8hgcdshk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "joann-employee"
+customregion = "us-east-1"
